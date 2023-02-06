5e0e3c142796e1d061d90a77863721b848c4abc7
msawant2310 <113842002+msawant2310@users.noreply.github.com>
2023-02-06 04:34:42 +0000

    Add files via upload

---
+++ AuE8230Spring23_MrunaliMahadeo_Sawant_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/msawant/AuE8230Spring23_MrunaliMahadeo_Sawant_ws/src
+# Build directory: /home/msawant/AuE8230Spring23_MrunaliMahadeo_Sawant_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_pkg")
